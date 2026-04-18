e223bd6bc975b0a576f8d6db000ed9b657ecfad3
Vo Xuan Tan <tanvophuyen@gmail.com>
2026-04-18 16:45:54 +0700

    xay sung co ban phan header

--- src/components/Layout/DefaultLayout/Header/index.js
+++ src/components/Layout/DefaultLayout/Header/index.js
@@ -1,5 +1,36 @@
+import classNames from 'classnames/bind';
+import styles from './Header.module.scss';
+import images from '../../../../assets/images';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleXmark } from '@fortawesome/free-solid-svg-icons';
+import { faCircleNotch, faSpinner, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+
+const cx = classNames.bind(styles);
 function Header() {
-    return <h2>Header</h2>;
+    return (
+        <header className={cx('wrapper')}>
+            <div className={cx('inner')}>
+                <div className={cx('logo')}>
+                    <img src={images.logo} alt="titolk" />
+                </div>
+                <div className={cx('search')}>
+                    <input placeholder="Search account and Video" spellCheck={false} className={cx('search-input')} />
+                    <button className={cx('clear')}>
+                        {/* close */ console.log(faCircleXmark)}
+
+                        <FontAwesomeIcon icon={faCircleXmark} />
+                    </button>
+                    {/* loading */}
+
+                    <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
+                    <button className={cx('search-btn')}>
+                        <FontAwesomeIcon icon={faMagnifyingGlass} />
+                    </button>
+                </div>
+                <div className={cx('actions')}>action</div>
+            </div>
+        </header>
+    );
 }
 
 export default Header;
--- src/components/Layout/DefaultLayout/index.js
+++ src/components/Layout/DefaultLayout/index.js
@@ -1,14 +1,16 @@
+import classNames from 'classnames/bind';
 import Header from './Header';
 import Sidebar from './Sidebar';
-import Upload from './Upload';
+import styles from './DefaultLayout.module.scss';
 
+const cx = classNames.bind(styles);
 function DefaultLayout({ children }) {
     return (
-        <div>
+        <div className={cx('wrapper')}>
             <Header />
-            <div className="container">
+            <div className={cx('container')}>
                 <Sidebar />
-                <div className="content">{children}</div>
+                <div className={cx('content')}>{children}</div>
             </div>
         </div>
     );
